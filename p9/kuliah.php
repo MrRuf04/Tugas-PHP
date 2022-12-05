12c83317889218c246e2eae5299dbedeceea775c
Amar Ma'ruf <107825981+MrRuf04@users.noreply.github.com>
2022-12-05 21:19:14 +0700

    Add files via upload

---
+++ p9/kuliah.php
@@ -0,0 +1,30 @@
+<?php 
+    //Tangkap request class_nilai.php
+    require_once 'class_nilai.php';
+
+    $ns1 = new NilaiSantri('Fulan',70);
+    $ns2 = new NilaiSantri('Badu',69);
+    $ns3 = new NilaiSantri('Usro',85);
+    $ns4 = new NilaiSantri('Fulan',40);
+    $ar_santri = [$ns1,$ns2,$ns3,$ns4];
+?>
+<table border="2" width="100%">
+    <thead>
+        <tr>
+            <th>No</th>
+            <th>Nama</th>
+            <th>Nilai</th>
+            <th>Hasil</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php 
+            $nomor = 1;
+            foreach($ar_santri as $san){
+                echo '<tr><td align="center">'.$nomor.'</td><td>'.$san->nama.'</td>';
+                echo '<td align="center">'.$san->nilai.'</td><td align="center">'.$san->getHasil().'</td></tr>';
+                $nomor++;
+            }
+        ?>
+    </tbody>
+</table>
