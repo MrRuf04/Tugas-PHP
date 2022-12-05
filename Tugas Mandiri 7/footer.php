2aa5f25fd3589d7fa3e51989eb2769f08973b0e6
Amar Ma'ruf <107825981+MrRuf04@users.noreply.github.com>
2022-12-05 21:17:47 +0700

    Add files via upload

---
+++ Tugas Mandiri 7/footer.php
@@ -0,0 +1,13 @@
+        <!-- Footer -->
+        <footer>
+            <div class="container">
+              <div class="row">
+                <div class="col-lg-12">
+                  <p>Copyright © 2022 Amar Ma'ruf ♡ Kota Dumai. 
+                  <br>Design: <a href="https://templatemo.com" target="_parent" title="free css templates">TemplateMo</a></p>
+                </div>
+              </div>
+            </div>
+          </footer>
+</body>
+</html>
---
+++ Tugas Mandiri 7/navbar.php
@@ -0,0 +1,13 @@
+<body>
+    <div class="container mt-lg-5">
+        <div class="main-body">
+        
+            <!-- Breadcrumb -->
+                <nav aria-label="breadcrumb" class="main-breadcrumb">
+                    <ol class="breadcrumb">
+                    <li class="breadcrumb-item"><a href="index.html">Home</a></li>
+                    <li class="breadcrumb-item"><a href="javascript:void(0)">User</a></li>
+                    <li class="breadcrumb-item active" aria-current="page">User Profile</li>
+                    </ol>
+                </nav>
+                <!-- /Breadcrumb -->
